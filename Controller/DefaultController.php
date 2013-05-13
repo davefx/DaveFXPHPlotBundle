777e18e38903343466846db7c627554ff82a100a
David Marín <davefx@gmail.com>
2013-05-13 18:58:47 +0300

    Update DefaultController.php

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace NoiseLabs\Bundle\NuSOAPBundle\Controller;
+namespace DaveFX\Bundle\PHPlotBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
-        return $this->render('NoiseLabsNuSOAPBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('DaveFXPHPlotBundle:Default:index.html.twig', array('name' => $name));
     }
 }

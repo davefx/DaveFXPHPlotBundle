47a16ae889ac090aac8f99769ac999df4817c1a0
David Marín <davefx@gmail.com>
2013-05-13 18:00:45 +0200

    Update and rename NoiseLabsNuSOAPExtension.php to DaveFXPHPlotExtension.php

--- DependencyInjection/DaveFXPHPlotExtension.php
+++ DependencyInjection/DaveFXPHPlotExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace NoiseLabs\Bundle\NuSOAPBundle\DependencyInjection;
+namespace DaveFX\Bundle\PHPlotBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class NoiseLabsNuSOAPExtension extends Extension
+class DaveFXPHPlotExtension extends Extension
 {
     /**
      * {@inheritDoc}
